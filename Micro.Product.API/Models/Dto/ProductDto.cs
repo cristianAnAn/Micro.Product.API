bd71c4dd30329de0ec2958caa105e074bb9bb4ab
cristianAnAn <22300509@uttt.edu.mx>
2025-05-21 11:22:28 -0600

    Agregacion del Dto de Product

--- Micro.Product.API/Models/Dto/ProductDto.cs
+++ Micro.Product.API/Models/Dto/ProductDto.cs
@@ -1,18 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Micro.Product.API.Models
+﻿namespace Micro.Product.API.Models.Dto
 {
-    public class Product
+    public class ProductDto
     {
-        [Key]
         public int ProductId { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Range(1, 100)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
         public string ImageLocalPath { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
